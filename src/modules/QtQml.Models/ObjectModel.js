56fe69175cf041c3c74fe95c3cdfe38a58ec97b1
zsfelber <zsfelber@gmail.com>
2017-01-22 21:39:25 +0100

    non-empty

---
+++ src/modules/QtQml.Models/ObjectModel.js
@@ -0,0 +1,16 @@
+QmlWeb.registerQmlType({
+  module: "QtQml.Models",
+  name: "ObjectModel",
+  versions: /^2\./,
+  baseClass: "QtQuick.Item",
+  properties: {
+    count: {type:"alias", path:["children", "length"], overrideType:"int", readOnly:true}
+  },
+}, class {
+  constructor(meta) {
+    QmlWeb.callSuper(this, meta);
+  }
+  get(index) {
+    return this.children[index];
+  }
+});
