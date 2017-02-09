58468695ffd6c5a240f181db7acd376cae1d3f5c
zsfelber <zsfelber@gmail.com>
2017-02-09 12:51:31 +0100

    non-empty

---
+++ src/modules/QtGraphicalEffects/DropShadow.js
@@ -0,0 +1,28 @@
+QmlWeb.registerQmlType({
+  module: "QtGraphicalEffects",
+  name: "DropShadow",
+  versions: /.*/,
+  baseClass: "Filter",
+   properties: {
+      radius: "real",
+      color: { type: "color", initialValue: "white" },
+      horizontalOffset: "int",
+      verticalOffset: "int"
+   }
+}, class {
+  constructor(meta) {
+    QmlWeb.superAndInitMeta(this, meta);
+
+   this.radiusChanged.connect(this, this.$update);
+   this.colorChanged.connect(this, this.$update);
+   this.horizontalOffsetChanged.connect(this, this.$update);
+   this.vericalOffsetChanged.connect(this, this.$update);
+  }
+  $updateFilterObject() {
+    this.$filterObject = {
+      transformType: "filter",
+      operation: "drop-shadow",
+      parameters: `${this.horizontalOffset}px ${this.verticalOffset}px ${this.radius}px ${this.radius}px ${this.color}`
+    };
+  }
+});
