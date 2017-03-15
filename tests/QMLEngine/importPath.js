96e9bf4656404e660796c75f7587b7cedcde51e8
zsfelber <zsfelber@gmail.com>
2017-03-15 18:36:35 +0100

    non-empty

--- tests/QMLEngine/importPath.js
+++ tests/QMLEngine/importPath.js
@@ -11,12 +11,12 @@ describe("QMLEngine.importPath", function() {
   it("addImportPath", function() {
     var engine = new QmlWeb.QMLEngine(this.div);
 
-    $$engine.addImportPath("http://example.org/path/to/qml");
+    engine.addImportPath("http://example.org/path/to/qml");
     expect(engine.userAddedImportPaths.length).toBe(1);
     expect(typeof engine.userAddedImportPaths[0]).toBe("string");
     expect(engine.userAddedImportPaths[0]).toBe("http://example.org/path/to/qml");
 
-    $$engine.addImportPath("http://example.org/second/path");
+    engine.addImportPath("http://example.org/second/path");
     expect(engine.userAddedImportPaths.length).toBe(2);
     expect(engine.userAddedImportPaths[0]).toBe("http://example.org/path/to/qml");
     expect(engine.userAddedImportPaths[1]).toBe("http://example.org/second/path");
@@ -25,20 +25,20 @@ describe("QMLEngine.importPath", function() {
   it("setImportPathList", function() {
     var engine = new QmlWeb.QMLEngine(this.div);
 
-    $$engine.addImportPath("http://example.org/path/to/qml");
-    $$engine.addImportPath("http://example.org/second/path");
+    engine.addImportPath("http://example.org/path/to/qml");
+    engine.addImportPath("http://example.org/second/path");
 
-    $$engine.setImportPathList([]);
+    engine.setImportPathList([]);
     expect(engine.userAddedImportPaths.length).toBe(0);
 
-    $$engine.addImportPath("http://example.org/path/to/qml");
+    engine.addImportPath("http://example.org/path/to/qml");
     expect(engine.userAddedImportPaths.length).toBe(1);
 
-    $$engine.setImportPathList(["http://example.org/path3"]);
+    engine.setImportPathList(["http://example.org/path3"]);
     expect(engine.userAddedImportPaths.length).toBe(1);
     expect(engine.userAddedImportPaths[0]).toBe("http://example.org/path3");
 
-    $$engine.setImportPathList(["http://example.org/4", "http://example.org/5"]);
+    engine.setImportPathList(["http://example.org/4", "http://example.org/5"]);
     expect(engine.userAddedImportPaths.length).toBe(2);
     expect(engine.userAddedImportPaths[0]).toBe("http://example.org/4");
     expect(engine.userAddedImportPaths[1]).toBe("http://example.org/5");
