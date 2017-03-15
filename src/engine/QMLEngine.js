aeb81aa9c1013ea6f61399fbb5ecff5c833b3a63
zsfelber <zsfelber@gmail.com>
2017-03-15 09:05:34 +0100

    non-empty

--- src/engine/QMLEngine.js
+++ src/engine/QMLEngine.js
@@ -639,7 +639,7 @@ function getEngine(chkengine) {
   if (!QmlWeb.$evaluatedObj) throw new QmlWeb.AssertionError("No engine.");
   const e = QmlWeb.$evaluatedObj.$engine;
   if (!e) throw new QmlWeb.AssertionError("No engine.");
-  if (chkengine && e !== chkengine) throw new QmlWeb.AssertionError("Another engine in context.");
+  if (chkengine instanceof QMLEngine && e !== chkengine) throw new QmlWeb.AssertionError("Another engine in context.");
   if (e.operationState & QmlWeb.QMLOperationState.Destroyed) throw new QmlWeb.AssertionError("Engine is destroyed.");
   return e;
 }
